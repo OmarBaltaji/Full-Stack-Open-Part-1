6a7be2aa3da7a838f20c4dd8393b8231bb044ee5
Omar Baltaji <omar@spark.boutique>
2021-10-11 18:58:17 +0300

    course information step 1

---
+++ courseInformation/src/Course Information/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = (props) => (
+  <h1>{props.courseName}</h1>
+);
+
+export default Header;
+
+
---
+++ courseInformation/src/Course Information/Total.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Total = (props) => (
+  <p>
+    Number of exercises {props.exercisesSum}
+  </p>
+);
+
+export default Total;
