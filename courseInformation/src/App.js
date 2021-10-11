91b1bca08e2e6fc8fa65147502d62b1b64b13b6c
Omar Baltaji <omar@spark.boutique>
2021-10-11 20:31:17 +0300

    course information step 5

--- courseInformation/src/App.js
+++ courseInformation/src/App.js
@@ -4,28 +4,29 @@ import Header from './Course Information/Header';
 import Total from './Course Information/Total';
 
 const App = () => {
-  const course = 'Half Stack application development';
-  
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10,
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7,
-    },
-    {
-      name: 'State of a component',
-      exercises: 14,
-    }
-  ];
+  const course = { 
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+      }
+    ]
+  };
 
   return (
     <div>
-      <Header courseName={course} />
-      <Content parts={parts} />
-      <Total parts = {parts} />
+      <Header courseName={course.name} />
+      <Content parts={course.parts} />
+      <Total parts = {course.parts} />
     </div>
   );
 };
